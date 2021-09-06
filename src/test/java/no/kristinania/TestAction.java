9feb44af8ae8fce56551c34fc135189123092ba8
ductien.dangnguyen <ductien.dangnguyen@gmail.com>
2021-09-06 13:45:40 +0200

    Initial commit

---
+++ src/test/java/no/kristinania/TestAction.java
@@ -0,0 +1,17 @@
+package no.kristinania;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestAction {
+    @Test
+    void testAction1() {
+        assertEquals("2", "2");
+    }
+
+    @Test
+    void testAction2() {
+        assertEquals("2", "3");
+    }
+}
